2bb5022292e70f843f8580462f4d5797a3927083
Keshav Tomar <keshavtomar53@gmail.com>
2023-06-16 15:23:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
 import './App.css';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import * as yup from "yup"
 
 function App() {
+  const defaultValue = {
+    fname: "",
+    course: "",
+    email: "",
+    phone: "",
+    gender: "",
+    termcondition: false,
+    addmission: "",
+  }
+  // const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+  const ValidationSchema = yup.object().shape({
+    fname: yup.string().min(3, "too short").max(20, "too much").required("Please enter the name"),
+    course: yup.string().min(2, "too short").max(4, "too much").required("Please enter the current course"),
+    email: yup.string().min(3, "too short").max(20, "too much").required("please enter the email").email("please vaild email"),
+    phone: yup.number().min(10, "valid number").min(10, "valid number").required("please enter phone number"),
+
+    gender: yup.string().required("please select gender "),
+    termcondition: yup.boolean().oneOf([true], "please accept terms and condition"),
+    addmission: yup.string().required("please choose only one way")
+  })
+
+  const handleSubmit = (values, resetForm) => {
+    console.log("form-value", values)
+    resetForm();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1 className='text-center my-4'>Form with Validation using Formik and yup</h1>
+      <div className='row mx-5 mt-4'>
+        <Formik
+          initialValues={defaultValue}
+          validationSchema={ValidationSchema}
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
+          <Form className='border-solid border-2 shadow p-3'>
+            <Field type="text" name="fname" placeholder="Enter the name" className="form-control mt-3" />
+            <p className='text-danger'>
+              <ErrorMessage name='fname' />
+            </p>
+            <Field type="text" name="course" placeholder="Enter the couse name" className="form-control  mt-3" />
+            <p className='text-danger'>
+              <ErrorMessage name='course' />
+            </p>
+            <Field type="email" name="email" placeholder="Enter the email" className="form-control  mt-3" />
+            <p className='text-danger'>
+              <ErrorMessage name='email' />
+            </p>
+            <Field type="text" name="phone" placeholder="Enter the phone" className="form-control  mt-3" />
+            <p className='text-danger'>
+              <ErrorMessage name='phone' />
+            </p>
+            {/* {Select box} */}
+            <Field component="select" name="gender" className="form-control">
+              <option value="" disabled>select</option>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+            </Field>
+            <p className='text-danger'>
+              <ErrorMessage name='gender' />
+            </p>
+            {/* checkbox */}
+            <label>
+              <Field type="checkbox" name="termcondition" className="form-inline m-1" />
+              I accept the term condition.
+            </label>
+            <p className='text-danger'>
+              <ErrorMessage name='termcondition' />
+            </p>
+            {/* Radio button */}
+            <div className='my-2'>
+              <label className='me-3'>
+                <Field type="radio" name="addmission" className="form-inline m-1" />
+                Direct addmission
+              </label>
+              <label>
+                <Field type="radio" name="addmission" className="form-inline m-1" />
+                Ref. to any Intrence
+              </label>
+            </div>
+            <p className='text-danger'>
+              <ErrorMessage name='addmission' />
+            </p>
+            <div className='text-center'>
+              <button type="submit" className='btn btn-warning '>Submit</button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 }
